7e328853e579c72d22db427d2e18c7b4ccffce45
shubham <33717931+shubhamdsm@users.noreply.github.com>
2021-01-23 19:51:46 +0530

    new, show & ask section done and insert to the dom

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 axios.get('https://api.hnpwa.com/v0/news/1.json')
     .then(function (response) {
-        console.log(response);
+        // console.log(response);
         news(response)
     })
     .catch(function (error) {
@@ -44,17 +44,89 @@ axios.get('https://api.hnpwa.com/v0/jobs/1.json')
     .catch(function(error){
         // console.log(error)
     })
-
+//INSERT AND ADD TO DOM
 function jobs(response){
     response.data.forEach(function(job){
        var div = document.createElement('div');
        div.className= 'container'
        div.innerHTML= `
        <ul class="list-group">
-        <li class="list-group-item">${job.title}<span style= 'padding-left: 1rem;'>(${job.domain})</span> <p>${job.time_ago} <bold>job</bold></p></li>
-       
+        <li class="list-group-item">${job.title}<span style= 'padding-left: 1rem;'>(${job.domain})</span> <p>${job.time_ago} </p>
+        
+        <a href=${job.url} class="btn btn-primary stretched-link">Job link</a>
+        </li>
+        
        </ul>
        `
        document.querySelector('#jobs').append(div)
     })
 }
+
+//API CALL FOR NEW
+axios.get('https://api.hnpwa.com/v0/newest/1.json')
+    .then(function(response){
+        // console.log(response.data)
+        newest(response)
+    })
+    .catch(function(error){
+        // console.log(error)
+    })
+//INSERT AND ADD TO DOM
+    function newest(response){
+        response.data.forEach(function(item){
+            var div = document.createElement('div');
+            div.className = 'container';
+            div.innerHTML = `
+            <a href= ${item.url} class="list-group-item list-group-item-action">
+                <div>
+                    <span class="badge rounded-pill bg-primary">${item.points} Points</span>
+                </div>
+                <div class="d-flex">
+                    <h5 class="mb-1">${item.title}</h5>
+                    <small class="text-muted" style='padding-left: 1rem;'>(${item.domain})</small>
+                </div>
+        
+                <p class="mb-1">by ${item.user}</p>
+                <small class="text-muted">${item.time_ago} | ${item.comments_count} comments</small>
+            </a>
+      `
+         document.querySelector('#new').appendChild(div)
+        })
+    }
+
+  
+//API CALL FOR SHOW 
+
+
+axios.get('https://api.hnpwa.com/v0/show/1.json')
+    .then(function(response){
+        // console.log(response.data)
+        show(response)
+    })
+    .catch(function(error){
+        // console.log(error)
+    })
+
+
+function show(response){
+    response.data.forEach(function(item){
+        var div = document.createElement('div');
+        div.className = 'container';
+        div.innerHTML = `
+        <a href= ${item.url} class="list-group-item list-group-item-action">
+            <div>
+                <span class="badge rounded-pill bg-primary">${item.points} Points</span>
+            </div>
+            <div class="d-flex">
+                <h5 class="mb-1">${item.title}</h5>
+                <small class="text-muted" style='padding-left: 1rem;'>(${item.domain})</small>
+            </div>
+    
+            <p class="mb-1">by ${item.user}</p>
+            <small class="text-muted">${item.time_ago} | ${item.comments_count} comments</small>
+        </a>
+  `
+     document.querySelector('#show').appendChild(div)
+    })
+}
+
